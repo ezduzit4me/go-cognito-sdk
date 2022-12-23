0d55e7051f68df885a1a1c35737b3c6594dd93dc
ezduzit4me <61637409+ezduzit4me@users.noreply.github.com>
2022-12-23 19:44:55 +1000

    error fix

--- clients/cognito.go
+++ clients/cognito.go
@@ -1,1 +1,38 @@
 package clients
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+)
+
+type CognitoClient interface {
+	SignUp(email string, password string) (error, string)
+}
+
+type awsCognitoClient struct {
+	cognitoClient *cognito.CognitoIdentityProvider
+	appClientId   string
+}
+
+func NewCognitoClient(cognitoRegion string, cognitoAppClientID string) CognitoClient {
+
+	conf := &aws.Config{Region: aws.String(cognitoRegion)}
+
+	sess, err := session.NewSession(conf)
+	client := cognito.New(sess)
+
+	if err != nil {
+		panic((err))
+	}
+
+	return &awsCognitoClient{
+		cognitoClient: client,
+		appClientId:   cognitoAppClientID,
+	}
+}
+
+func (ctx *awsCognitoClient) SignUp(email string, password string) (error, string) {
+
+	return nil, "Hello Golang"
+}
--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/ezduzit4me/go-cognito-sdk/clients"
+)
+
+func main() {
+
+	cognitoClient := clients.NewCognitoClient("", "")
+	err, result := cognitoClient.SignUp("geoffreyWarrener@gmail.com", "test")
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("result:" + fmt.Sprintf("%s", result))
+}

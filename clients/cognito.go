2fda553f35f81392c8e5a85c3603b3be18d47211
ezduzit4me <61637409+ezduzit4me@users.noreply.github.com>
2022-12-24 16:17:06 +1000

    working sign up

--- clients/cognito.go
+++ clients/cognito.go
@@ -1,8 +1,6 @@
 package clients
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
@@ -10,7 +8,8 @@ import (
 
 type CognitoClient interface {
 	SignUp(email string, password string) (error, string)
-	ConfirmSignUp(email string, code string) (error,string)
+	ConfirmSignUp(email string, code string) (error, string)
+	// SignIn(email string, code string) (error, string)
 }
 
 type awsCognitoClient struct {
@@ -36,37 +35,45 @@ func NewCognitoClient(cognitoRegion string, cognitoAppClientID string) CognitoCl
 }
 
 func (ctx *awsCognitoClient) SignUp(email string, password string) (error, string) {
-	user:= &cognito.SignUpInput{
-      Username: aws.String(email),
-      Password: aws.String(password),
-	  ClientId: aws.String(ctx.appClientId),
-	  UserAttributes: []*cognitoAttributeType {
-{
-		Name: aws.String("email"),
-		Value: awsString(email),
-	  },
-	},
-result,err:= ctx.cognitoClient.Signup(user)
- if err !=nil {
-	return err, ""
- }
+	user := &cognito.SignUpInput{
+		Username: aws.String(email),
+		Password: aws.String(password),
+		ClientId: aws.String(ctx.appClientId),
+		UserAttributes: []*cognito.AttributeType{
+			{
+				Name:  aws.String("email"),
+				Value: aws.String(email),
+			},
+		},
+	}
+	result, err := ctx.cognitoClient.SignUp(user)
+	if err != nil {
+		return err, ""
+	}
 	return nil, result.String()
 }
 
-func (ctx *aws.cognitoClient) ConfirmSignUp(email string,code string) (error,string){
-confirmSignUpInput:= &cognito.ConfirmSignUpInput{
-	Username: aws.String(email),
-	ConfirmationCode: aws.String(code),
-	ClientId: aws.String(ctx.appClientId),
-}
- result, err:= ctx.cognitoClient.ConfirmSignUp
+func (ctx *awsCognitoClient) ConfirmSignUp(email string, code string) (error, string) {
+	confirmSignUpInput := &cognito.ConfirmSignUpInput{
+		Username:         aws.String(email),
+		ConfirmationCode: aws.String(code),
+		ClientId:         aws.String(ctx.appClientId),
+	}
+	result, err := ctx.cognitoClient.ConfirmSignUp(confirmSignUpInput)
 
-	if err!= nil{
+	if err != nil {
 
 		return err, ""
 	}
 
-	return nil,result.String()
+	return nil, result.String()
 
-	
 }
+
+//func (ctx *awsCognitoClient) SignIn(email string, code string) (error, string) {
+//	initiateAuthInput := &cognito.InitiateAuthInput()
+
+//	result, err := ctx.cognitoClient.InitiateAuth()
+
+//return nil, result.String()
+// }
--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 
 	cognitoClient := clients.NewCognitoClient("ap-southeast-2", "26c2pgelskg6cq61htpck65ii3")
-	err, result := cognitoClient.SignUp("iran.rourke@dropsin.net", "test")
+	err, result := cognitoClient.SignUp("iran.rourke@dropsin.net", "5252/66Lam-4005")
 
 	if err != nil {
 		fmt.Print("Error" + fmt.Sprint("%s", err))

f21d5d29b8bc0a80471c77c792b2ec317803a175
ezduzit4me <61637409+ezduzit4me@users.noreply.github.com>
2022-12-31 15:39:53 +1000

    not save check

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 func main() {
 
 	cognitoClient := clients.NewCognitoClient("ap-southeast-2", "26c2pgelskg6cq61htpck65ii3")
-	//err, result, initiateAuthOutput := cognitoClient.SignUp("iran.rourke@dropsin.net", "5252/66Lam-4005")
-	err, result, initiateAuthOutput := cognitoClient.SignIn("iran.rourke@dropsin.net", "5252/66Lam-4005")
-
+	//err, result := cognitoClient.SignUp("iran.rourke@dropsin.net", "5252/66Lam-4005")
+	err, result, initiateAuthOutput := cognitoClient.SignIn("fred@fred.com", "5252/66Lam-4005")
+	//err, result := cognitoClient.ConfirmSignUp("iran.rourke@dropsin.net", "")
 	if err != nil {
 		fmt.Print("Error" + fmt.Sprint("%s", err))
 		panic(err)
